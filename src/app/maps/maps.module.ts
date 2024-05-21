90c5d1ee0bc1ca46d4e99a20b1d6890ce8695e8d
John <jeobando11@hotmail.com>
2024-05-21 16:24:59 -0500

    Aplicando standalone

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -6,24 +6,30 @@ import * as mapboxgl from 'mapbox-gl';
   'pk.eyJ1Ijoiam9obmVyaWtjIiwiYSI6ImNrb3VmeXh3djBpNTkyd3BoODAycTk4bmYifQ.dRn9kQ4Ql6GtSUidW-PCVg';
 
 import { MapsRoutingModule } from './maps-routing.module';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
-import { MiniMapComponent } from './components/mini-map/mini-map.component';
-import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
+
+import { CounterAloneComponent } from '../alone/components/counter-alone/counter-alone.component';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
+import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
+import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
+import { SideMenuComponent } from '../alone/components/side-menu/side-menu.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
 
 @NgModule({
   declarations: [
-    SideMenuComponent,
-    MiniMapComponent,
-    MapsLayoutComponent,
     FullScreenPageComponent,
+    MapsLayoutComponent,
     MarkersPageComponent,
+    MiniMapComponent,
     PropertiesPageComponent,
     ZoomRangePageComponent,
   ],
-  imports: [CommonModule, MapsRoutingModule],
+  imports: [
+    CommonModule,
+    CounterAloneComponent,
+    MapsRoutingModule,
+    SideMenuComponent,
+  ],
 })
 export class MapsModule {}
